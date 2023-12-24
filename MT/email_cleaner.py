bca98488c936195414135e0b0ab040830021f1ab
TimCauss <t.caussignac@gmail.com>
2023-12-24 18:15:22 +0100

    Addins some coms

--- MT/email_cleaner.py
+++ MT/email_cleaner.py
@@ -1,6 +1,7 @@
 from Libs.cleaners import clean_html, clean_email, clean_url
 
-
+## Refactor this module, it's for clean and prepare a mail to be read by an IA model.
+## Maybe use dict for listing function by cleaned object
 
 
 def clean_email(email):
