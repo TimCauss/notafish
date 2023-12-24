3a74feb0143b568d3620321c8c8d881c244000d8
TimCauss <t.caussignac@gmail.com>
2023-12-24 18:40:31 +0100

    Remove:

--- Libs/cleaners.py
+++ Libs/cleaners.py
@@ -6,12 +6,23 @@ from bs4 import BeautifulSoup
 
 def clean_email(value):
     return re.sub(r'\S*@\S*\s?', '', value)
-
 def clean_html(value):
     return BeautifulSoup(value, "html.parser").get_text()
-
 def clean_url(value):
     return re.sub(r'http\S+', '', value)
-
 def clean_char(value):
     pass #Define cleaner for spec characters
+
+
+## Refactor this module, it's for clean and prepare a mail to be read by an IA model.
+## Maybe use dict for listing function by cleaned object
+
+
+standardization_functions = {
+    "email" : {
+        "clean": [clean_html, clean_url, clean_email],
+        "format": [],
+        "standardize": []
+    }
+    
+}
