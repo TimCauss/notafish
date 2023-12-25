bb7b385d354bae6549a39044edf4a5ecec8a0d83
TimCauss <t.caussignac@gmail.com>
2023-12-25 15:10:22 +0100

    Update; clean_email function updated for import in unittest

--- tests/libs/test_cleaners.py
+++ tests/libs/test_cleaners.py
@@ -1,5 +1,12 @@
 import unittest
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from Libs.cleaners import clean_email
+
+
 # Correct the imports
 
 
