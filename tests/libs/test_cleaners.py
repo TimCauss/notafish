044cb4a8afa1ed96053b843a3e11c2348858fa49
TimCauss <t.caussignac@gmail.com>
2023-12-25 15:49:27 +0100

    updated unit test v1

--- tests/libs/test_cleaners.py
+++ tests/libs/test_cleaners.py
@@ -1,22 +1,19 @@
-import unittest
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-
 from Libs.cleaners import clean_email
-
-
-# Correct the imports
+import unittest
 
 
 class TestCleanemail(unittest.TestCase):
 
+    test_cases = {
+        "My email is example@example.com thank you.": "My email is thank you.",
+        "example@example.com is my email.": "is my email.",
+        "test.com is not an email.": "test.com is not an email.",
+    }
+
     def test_clean_email(self):
-        self.assertEqual(clean_email(
-            "My email is example@example.com thank you."), "My email is thank you.")
-        self.assertEqual(clean_email(
-            "example.com is my email."), " is my email.")
+        for input_text, expected_output in self.test_cases.items():
+            with self.subTest(input_text=input_text):
+                self.assertEqual(clean_email(input_text), expected_output)
 
 
 if __name__ == '__main__':
